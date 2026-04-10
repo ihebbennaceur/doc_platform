e3681004f759ac9611f43ac46ffaf3b0a458d5f0
ihebbennaceur <ihebbennaceur6@gmail.com>
2026-04-10 08:33:57 +0200

    Remove DocCheck service - now in separate repo

--- django/doccheck_service/doccheck_service/wsgi.py
+++
@@ -1,7 +0,0 @@
-"""WSGI config for doccheck_service project."""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doccheck_service.settings')
-
-application = get_wsgi_application()
